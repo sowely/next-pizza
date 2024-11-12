68d574ccf7ccdee52d1581b9e75a108eee7eb739
Pikulina Darya <pikulinadasha99@gmail.com>
2024-11-13 02:58:17 +0700

    add parallel route / modal

---
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,6 @@
+import { Container, Filters, ProductsGroupList, Title, TopBar } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+
+export default async function ProductModalPage() {
+    return (<h1>aboba</h1>);
+}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,15 +6,18 @@ export const metadata: Metadata = {
     description: 'Next Pizza',
 };
 
-export default function RootLayout({
+export default function HomeLayout({
     children,
+    modal,
 }: Readonly<{
     children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
     return (
         <main className='min-h-screen'>
             <Header />
             {children}
+            {modal}
         </main>
     );
 }
