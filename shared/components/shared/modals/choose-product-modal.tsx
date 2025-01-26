627455b8a923a2d21607fe6c844aadf21caf2f96
Pikulina Darya <pikulinadasha99@gmail.com>
2025-01-26 23:50:08 +0700

    notfix console errors

--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { cn } from '@/shared/lib/utils';
-import { Dialog, DialogContent } from '@/shared/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/shared/components/ui/dialog';
 import { ChoosePizzaForm, ChooseProductForm } from '..';
 import { useRouter } from 'next/navigation';
 import { ProductWithRelations } from '@/@types/prisma';
@@ -18,6 +18,10 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
 
     return (
         <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
+            {/* <VisuallyHidden.Root>
+                <DialogTitle></DialogTitle>
+                <DialogDescription></DialogDescription>
+            </VisuallyHidden.Root> */}
             <DialogContent className={cn('p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden', className)}>
                 {
                     isPizzaForm ? (
