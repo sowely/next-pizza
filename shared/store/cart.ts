a8db2a75f3eaad77ff445074e462f783478bd123
Pikulina Darya <pikulinadasha99@gmail.com>
2025-05-11 23:00:50 +0700

    fix import path

--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { Api } from "../services/api-client";
 import { getCartDetails } from "../lib";
-
+import { CartStateItem } from "../lib/get-cart-details";
 
 export interface CartState {
 	loading: boolean;
